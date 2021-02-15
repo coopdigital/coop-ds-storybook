bb0af483b3a1d3152b3498b329ef88e9031d4b52
Dale Hudson <dalefrontend@gmail.com>
2021-02-15 20:09:21 +0000

    test: update tests for editorialCard

--- src/components/EditorialCard/__tests__/EditorialCard.test.js
+++ src/components/EditorialCard/__tests__/EditorialCard.test.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { graphql } from "gatsby";
-
 import EditorialCard from "../EditorialCard";
+import ResponsiveImage from "../../ResponsiveImage";
 
-//mock data
+const MediaComponent = () => {
+  const src =
+    "https://images.ctfassets.net/bffxiku554r1/7XDwxNP8ioupeMdJZxNS8/d23d2b802d6be0ecbcb7abd1ff33b159/coop-supporting-local-communities.png?fm=png&q=60&w=600&h=338";
+  const alt = "How Co-op is supporting local communities";
+  const figureClass = 'coop-c-editorialcard__media';
+  const pictureClass = 'coop-c-editorialcard__image';
+  return <ResponsiveImage src={src} alt={alt} figureClass={figureClass} pictureClass={pictureClass} />;
+};
+
 
 describe("Editorial Card test", () => {
   it("renders correctly", () => {
@@ -65,11 +72,38 @@ describe("Editorial Card test", () => {
     );
   });
 
-  // test gatsby-image <Img/> component??
-
   // it should render with image when prop has value
-  // it should render without error when no image is supplied
+  it("it should render with image when prop has value", () => {
+    const { container } = render(
+      <EditorialCard
+        title="Editorial card horizontal"
+        link="#"
+        content="content test"
+        mediaComponent={<MediaComponent/>}
+      />
+    );
+    const image = container.querySelector('.coop-c-image')
+    expect(image).toHaveClass(
+      "coop-c-image"
+    );
+  });
 
-  // test gatsby <Link/>
+  // it should render without error when no image is supplied
+  it("it should render without error when no image is supplied", () => {
+    const { container } = render(
+      <EditorialCard
+        title="Editorial card horizontal"
+        link="#"
+        content="content test"
+      />
+    );
+    const image = container.querySelector('.coop-c-image')
+    expect(image).toBeNull();
+    expect(container.firstChild).toHaveClass(
+      "coop-c-editorialcard"
+    );
+  });
   // it should error when no link is supplied
+
+  //Accessibility tests
 });
