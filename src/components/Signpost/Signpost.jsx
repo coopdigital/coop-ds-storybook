7f000c01572bbd9eb1b323e27d61c8e4fe3f7700
Dale Hudson <dalefrontend@gmail.com>
2021-02-16 12:11:01 +0000

    feat: add signpost stories and start signpost tests

--- src/components/Signpost/Signpost.jsx
+++ src/components/Signpost/Signpost.jsx
@@ -18,52 +18,8 @@ const Signpost = ({ title, url, mediaComponent }) => {
           data-contentparent=""
           data-linktext=""
         >
-          {mediaComponent && (
-            <figure class="coop-c-signpost__media">
-              <picture class="coop-c-signpost__image">
-                <source
-                  media="(min-width: 48em)"
-                  srcset="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=webp&amp;fit=thumb&amp;q=60&amp;w=360&amp;h=203 1x,
-        //images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=webp&amp;fit=thumb&amp;q=60&amp;w=720&amp;h=406 2x"
-                  type="image/webp"
-                />
-                <source
-                  media="(min-width: 37.5em)"
-                  srcset="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=webp&amp;fit=thumb&amp;q=60&amp;w=751&amp;h=423 1x,
-        //images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=webp&amp;fit=thumb&amp;q=60&amp;w=1502&amp;h=846 2x"
-                  type="image/webp"
-                />
-                <source
-                  media="(min-width: 25.9375em)"
-                  srcset="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=webp&amp;fit=thumb&amp;q=60&amp;w=583&amp;h=329 1x,
-        //images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=webp&amp;fit=thumb&amp;q=60&amp;w=1166&amp;h=658 2x"
-                  type="image/webp"
-                />
-                <source
-                  media="(min-width: 48em)"
-                  srcset="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;fl=progressive&amp;q=60&amp;w=360&amp;h=203 1x,
-        //images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;fl=progressive&amp;q=60&amp;w=720&amp;h=406 2x"
-                  type="image/jpeg"
-                />
-                <source
-                  media="(min-width: 37.5em)"
-                  srcset="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;fl=progressive&amp;q=60&amp;w=751&amp;h=423 1x,
-        //images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;fl=progressive&amp;q=60&amp;w=1502&amp;h=846 2x"
-                  type="image/jpeg"
-                />
-                <source
-                  media="(min-width: 25.9375em)"
-                  srcset="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;fl=progressive&amp;q=60&amp;w=583&amp;h=329 1x,
-        //images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;fl=progressive&amp;q=60&amp;w=1166&amp;h=658 2x"
-                  type="image/jpeg"
-                />
-                <img
-                  src="//images.ctfassets.net/bffxiku554r1/4wuuKMjqUbsIJL7gl65A7I/3ebd189f497edaf54091615e67203910/Memb.png?fm=jpg&amp;fit=thumb&amp;q=60&amp;w=751&amp;h=423"
-                  alt="Membership"
-                />
-              </picture>
-            </figure>
-          )}
+          { mediaComponent && ( mediaComponent ) }
+
           <div class="coop-c-signpost__content">
             <h3 class="coop-c-signpost__title">{title}</h3>
             <span class="coop-c-signpost__icon" aria-hidden="true">
@@ -90,7 +46,7 @@ Signpost.defaultProps = {
 Signpost.propTypes = {
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  mediaComponent: PropTypes.string.isOptional,
+  mediaComponent: PropTypes.node,
   dataContentType: PropTypes.string.isOptional,
   dataContentParent: PropTypes.string.isOptional,
   dataLinkText: PropTypes.string.isOptional,
---
+++ src/components/Signpost/__tests__/Signpost.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { mount } from "enzyme";
+import renderer from "react-test-renderer";
+import Signpost from "../index";
+
+const MediaComponent = () => {
+  const src =
+    "https://images.ctfassets.net/bffxiku554r1/7XDwxNP8ioupeMdJZxNS8/d23d2b802d6be0ecbcb7abd1ff33b159/coop-supporting-local-communities.png?fm=png&q=60&w=600&h=338";
+  const alt = "How Co-op is supporting local communities";
+  const figureClass = "coop-c-signpost__media";
+  const pictureClass = "coop-c-signpost__image";
+  return (
+    <ResponsiveImage
+      src={src}
+      alt={alt}
+      figureClass={figureClass}
+      pictureClass={pictureClass}
+    />
+  );
+};
+
+describe("Signpost", () => {
+  it("should render correctly", () => {
+    const wrapper = mount(<Signpost src="src" alt="alt" />);
+    expect(() => wrapper.unmount()).not.toThrow();
+
+    const tree = renderer.create(<Signpost />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  //it should take a child component as mediacomponent without error
+
+  //it should render the ImageComponent is used
+
+  //it should carry a link
+
+  //it should error without link or title
+});
