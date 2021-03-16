bb6db8681dea2da5def11111db5383cea35fe43c
Dale Hudson <dalefrontend@gmail.com>
2021-03-16 13:19:44 +0000

    code: update classnames on blockquote

--- src/components/BlockQuote/BlockQuote.js
+++ src/components/BlockQuote/BlockQuote.js
@@ -4,9 +4,9 @@ import "./blockQuote.scss";
 
 const BlockQuote = ({ className, citation, children }) => {
   return (
-    <blockquote>
+    <blockquote className="coop-t-blockquote">
       <p className={className}>{children}</p>
-      <cite>{citation}</cite>
+      <cite className="coop-t-blockquote__citation">{citation}</cite>
     </blockquote>
   );
 };
